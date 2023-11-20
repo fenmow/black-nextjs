e3be3e2656bb330ec117e27d2279cc65c1f31e89
fenmow <lucas170105@outlook.com>
2023-11-20 01:18:13 -0300

    added environment variable for server side rendering

--- src/pages/dynamic.tsx
+++ src/pages/dynamic.tsx
@@ -1,7 +1,35 @@
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
+import { useState, useEffect } from "react";
 import { Col, Container, Row } from "reactstrap";
 
-const Dynamic: NextPage = () => {
+interface ApiResponse {
+  name: string
+  timestamp: Date
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const serverSideData: ApiResponse = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/hello`).then(res => res.json())
+  return {
+    props: {
+      serverSideData
+    }
+  }
+}
+
+const Dynamic: NextPage = (props: {
+  serverSideData?: ApiResponse
+}) => {
+  const [clientSideData, setClientSideData] = useState<ApiResponse>()
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    const data: ApiResponse = await fetch("/api/hello").then(res => res.json())
+    setClientSideData(data)
+  }
+  
   return (
     <Container className="main">
       <h1 className="my-5">Renderizações do Next.js</h1>
@@ -10,10 +38,12 @@ const Dynamic: NextPage = () => {
 
         <Col>
           <h3>Renderização do servidor: </h3>
+          <h2>{props.serverSideData?.timestamp.toString()}</h2>
         </Col>
 
         <Col>
-        <h3>Renderização do cliente: </h3>
+          <h3>Renderização do cliente: </h3>
+          <h2>{clientSideData?.timestamp.toString()}</h2>
         </Col>
         
       </Row>
