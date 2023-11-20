e28789324eb5de91ecd851f492ca15b84d385525
fenmow <lucas170105@outlook.com>
2023-11-20 00:55:40 -0300

    First commit

---
+++ src/pages/products.tsx
@@ -0,0 +1,22 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const Products: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Nossos Produtos</title>
+        <meta name="description" content="Conheça os nossos produtos"/>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Nossos Produtos
+        </h1>
+      </main>
+    </>
+  )
+}
+
+export default Products
